d5780b9b588453bce2eb23486d1ca09f287dd24e
David <davidlsmith524@gmail.com>
2026-03-03 20:46:14 -0500

    Finished 3--removing an item from the dictionary. Find code is getting to be a lot. See need of either different functions or classes

--- to_do_list/to_do_list.py
+++ to_do_list/to_do_list.py
@@ -1,21 +1,17 @@
 to_do_items = {}
 
-# Main menu items
-
-
-
 def to_do_list():
 
     while True:
-    
+        # Main menu items
         print("\nChoose or enter a to do list item you'd like to complete:")
         print("\n1. View items")
         print("\n2. Add item")
         print("\n3. Remove item")
         print("\n4. Exit the app")
         answer = input("\nPlease enter your choice: ")
-
     
+        # User response options
         if answer == "1":
             print("Here are your current to do items: ")
             print(f"You have {len(to_do_items)}, they are: {to_do_items}")
@@ -27,15 +23,24 @@ def to_do_list():
             position = input("What position would you like to put it? Please enter numbers only!\n")
             try:
                 value = int(position)
-                to_do_items[value] = add_item
+                if position in to_do_items:
+                    to_do_items[position].append(add_item)
+                else:
+                    to_do_items[value] = add_item
             except ValueError:
                 print("You must enter a number!")
             print(f"You added {add_item} to position {position}, now do that task!")
             continue
 
         elif answer == "3":
-            print("What task would you like to remove?")
-            
+            print("What task would you like to remove? Please enter position only!")
+            remove_item = input("")
+            removed_item = to_do_items.pop(remove_item, None)
+            if remove_item is not None:
+                print(f"Removed {position, add_item} from your to do list! Good job!")
+            else:
+                print(f"That item {position, add_item} is not in your list!")
+
         elif answer == "4":
             print("Are you sure you would like to quit?")
 
